36176eb144ce57068b7443be511d43ba8bcde71f
noahjacobs <noahjacobs@icloud.com>
2025-12-22 12:33:04 -0500

    rm api and yarn files

--- apps/api/v2/src/modules/auth/decorators/pbac/pbac.decorator.ts
+++
@@ -1,5 +0,0 @@
-import { Reflector } from "@nestjs/core";
-
-import type { PermissionString } from "@calcom/platform-libraries/pbac";
-
-export const Pbac = Reflector.createDecorator<PermissionString[]>();
